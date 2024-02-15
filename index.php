b976a77b3f85357b765e5bfad738823b6c248d52
Cloud <ruben.as532@gmail.com>
2024-02-15 10:55:07 +0100

    Added bugfix for case 0 0

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
 
 function isFigureInNumber(int $number, int $figure): bool {
+    
+    if($number == 0 && $figure == 0) return true;
+
     while($number > 0) {
         if($number % 10 == $figure) return true;
         $number /= 10;
@@ -8,8 +11,8 @@ function isFigureInNumber(int $number, int $figure): bool {
     return false;
 }
 
-$number = 3772341;
-$figure = 7;
+$number = 1;
+$figure = 1;
 
 $msg = "La xifra ".$figure;
 if(!isFigureInNumber($number,$figure)) {
